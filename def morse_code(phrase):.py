386ba2430d2812c2f022b44fa3b93bb74db49f15
alibozahra <aa2201049@tkh.edu.eg>
2023-11-29 03:08:29 +0200

    Update def morse_code(phrase):.py
    function

--- def morse_code(phrase):.py
+++ def morse_code(phrase):.py
@@ -30,4 +30,22 @@ def morse_code(phrase):
         ' ': ' '
     }
 
-   
+    # Convert the phrase to lowercase and remove spaces
+    phrase = phrase.lower().replace(" ", "")
+
+    # Initialize the encrypted or decrypted phrase
+    encoded_phrase = ""
+
+    # Iterate through each character in the phrase
+    for char in phrase:
+        # Check if the character exists in the Morse code dictionary
+        if char in morse_code_dict:
+            # Add the corresponding Morse code representation to the encoded phrase
+            encoded_phrase += morse_code_dict[char] + " "
+        else:
+            # If the character is not found, ignore it
+            pass
+
+    # Return the encoded or decrypted phrase
+    return encoded_phrase
+
